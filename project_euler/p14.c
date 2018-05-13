7721008521f7e5e3195fdf10dad83714a3546cf7
Myyura <zz940521@gmail.com>
2018-05-14 02:31:46 +0900

    Problem 14 ~ 15

---
+++ project_euler/p14.c
@@ -0,0 +1,35 @@
+/* Longest Collatz sequence */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int64_t collatz_seq(int64_t n) {
+    return n % 2 == 0 ? n / 2 : 3 * n + 1;
+}
+
+int64_t count_collatz_seq(int64_t n) {
+    int64_t count = 2;
+    for (; collatz_seq(n) != 1; count++, n = collatz_seq(n))
+        ;
+
+    return count;
+}
+
+int64_t max_collatz_seq(void) {
+    int64_t max_seq = 0;
+    int64_t start_number = 1;
+    for (int64_t i = 1000000; i >= 1; i--) {
+        int64_t cur_seq = count_collatz_seq(i);
+        if (max_seq < cur_seq) {
+            max_seq = cur_seq;
+            start_number = i;
+        }
+    }
+
+    return start_number;
+}
+
+int main(void) {
+    printf("%lld\n", max_collatz_seq());
+    return 0;
+}
---
+++ project_euler/p15.c
@@ -0,0 +1,47 @@
+/* Lattice paths */
+
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+    https://en.wikipedia.org/wiki/Lattice_path
+    C(n, 2n)
+*/
+
+int64_t num_lattice_paths_combinatorial(int n) {
+    int64_t result = 1;
+    for (int i = n + 1, j = 1; i <= 2 * n; i++, j++) {
+        result *= i;
+        result /= j;
+    }
+
+    return result;
+}
+
+/*
+    To get to position [i, j] in one step, we must already in position 
+    [i - 1, j] or [i, j - 1].
+*/
+
+int64_t num_lattice_paths_dp(int n) {
+    int64_t grid[n + 1][n + 1];
+
+    for (int i = 0; i <= n; i++) {
+        grid[0][i] = 1;
+        grid[i][0] = 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+        }
+    }
+
+    return grid[n][n];
+}
+
+int main(void) {
+    printf("%lld\n", num_lattice_paths_combinatorial(20));
+    printf("%lld\n", num_lattice_paths_dp(20));
+    return 0;
+}
