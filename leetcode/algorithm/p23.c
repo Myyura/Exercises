641bed5c7bc7b26edf3aca35dff6e117da1e51fb
Myyura <zz940521@gmail.com>
2018-06-12 16:55:23 +0900

    Leetcode algorithm 23

--- leetcode/algorithm/p23.c
+++ leetcode/algorithm/p23.c
@@ -4,7 +4,8 @@
  */
 
 /**
- * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and 
+ * describe its complexity.
  * 
  * Example:
  * Input:
