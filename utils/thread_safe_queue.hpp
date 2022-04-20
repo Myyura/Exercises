f6db0a0dafb18bcb52822f6919545ce62ad75ec6
Myyura <zz940521@gmail.com>
2022-04-20 20:11:15 +0800

    Update thread_safe_queue.hpp

--- utils/thread_safe_queue.hpp
+++ utils/thread_safe_queue.hpp
@@ -1,3 +1,7 @@
+//
+// Created by Myyura on 2022/04/20.
+//
+
 #ifndef THREAD_SAFE_QUEUE_HPP
 #define THREAD_SAFE_QUEUE_HPP
 
@@ -13,11 +17,6 @@
 #include <queue>
 
 #include <unistd.h>
-
-
-enum QResult {
-    OK, CLOSED
-};
 
 template <typename T>
 class ThreadSafeQueue {
@@ -36,120 +35,101 @@ private:
     std::condition_variable _cv_push, _cv_pop;
     // The mutex for locking the queue.
     mutable std::mutex _mutex;
-    // The list that the queue is implemented with.
-    std::list<T> _list;
-    // How many producers are using it
-    int _n_producer;
-    int _n_closed_producer;
+    // The queue
+    std::queue<T> _queue;
+    // Mode, 0: drop, 1: wait when queue is full
+    int _mode;
 
 public:
     // Initialize the queue with a maximal size.
-    explicit ThreadSafeQueue(int n_producer, size_t max_size = 0): \
-        _state(State::OPEN), _current_size(0), _n_producer(n_producer), _n_closed_producer(0), _max_size(max_size) {}
+    explicit ThreadSafeQueue(size_t max_size = 1, int mode = 0): \
+        _state(State::OPEN), _current_size(0), _max_size(max_size), _mode(mode) 
+    {
+        if (max_size <= 0) {
+            throw std::invalid_argument("max_size must be greater than 0.")
+        }
+    }
 
     ~ThreadSafeQueue() = default;
 
     // 禁止拷贝
     ThreadSafeQueue& operator=(const ThreadSafeQueue&) = delete;
 
+    bool full(void) { return _max_size >= _current_size; }
+    bool empty(void) { return _current_size <= 0; }
+
     // 压入元素
     void push(T const& v) {
-        decltype(_list) temp_list;
-        temp_list.push_back(v);
-
-        {
-            std::unique_lock<std::mutex> lock(_mutex);
-
-            // 等待队列有空位
-            while (_current_size == _max_size && _state == State::OPEN) {
-                _cv_push.wait(lock) ;
+        std::unique_lock<std::mutex> lock(_mutex);
+        if (full()) {
+            if (_mode == 0) {
+                // drop mode, 直接pop掉一个元素
+                _queue.pop();
+                _current_size--;
+            } else if (_mode == 1) {
+                // wait mode, 等待队列有空位又或者队列在其他地方被关闭
+                _cv_push.wait(lock, [&]{ return !full() || _state != State::OPEN; })
             }
-
-            if (_state == State::CLOSED) {
-                throw std::runtime_error("Trying to push to a closed queue.");
-            }
-
-            // 压入队列
-            _current_size += 1;
-            _list.splice(_list.end(), temp_list, temp_list.begin());
-
-            // 唤醒取数据的线程
-            _cv_pop.notify_one();
         }
+
+        if (_state == State::CLOSED) {
+            // throw std::runtime_error("Trying to push to a closed queue.");
+            return;
+        }
+
+        _current_size++;
+        _queue.push(v)
+
+        _cv_pop.notify_one();
     }
 
-    // 压入元素（右值引用）
+    // 压入元素
     void push(T && v) {
-        decltype(_list) temp_list;
-        temp_list.push_back(v);
-
-        {
-            std::unique_lock<std::mutex> lock(_mutex);
-
-            // 等待队列有空位
-            while (_current_size == _max_size && _state == State::OPEN) {
-                _cv_push.wait(lock) ;
+        std::unique_lock<std::mutex> lock(_mutex);
+        if (full()) {
+            if (_mode == 0) {
+                // drop mode, 直接pop掉一个元素
+                _queue.pop();
+                _current_size--;
+            } else if (_mode == 1) {
+                // wait mode, 等待队列有空位又或者队列在其他地方被关闭
+                _cv_push.wait(lock, [&]{ return !full() || _state != State::OPEN; })
             }
-
-            if (_state == State::CLOSED) {
-                throw std::runtime_error("Trying to push to a closed queue.");
-            }
-
-            // 压入队列
-            _current_size += 1;
-            _list.splice(_list.end(), temp_list, temp_list.begin());
-
-            // 唤醒取数据的线程
-            _cv_pop.notify_one();
         }
+
+        if (_state == State::CLOSED) {
+            // throw std::runtime_error("Trying to push to a closed queue.");
+            return;
+        }
+
+        _current_size++;
+        _queue.push(v)
+
+        _cv_pop.notify_one();
     }
 
-    // 取出元素
-    QResult pop(T& v) {
-        decltype(_list) temp_list;
-
-        {
-            std::unique_lock<std::mutex> lock(_mutex);
-
-            // 等待队列中存在数据
-            while (_list.empty() && _state != State::CLOSED) {
-                _cv_pop.wait(lock);
-            }
-
-            // 若队列已关闭且为空，则没有值可取，返回CLOSED
-            if (_list.empty() && _state == State::CLOSED) {
-                return CLOSED;
-            }
-
-            // 取出数据
-            _current_size -= 1;
-            temp_list.splice(temp_list.begin(), _list, std::prev(_list.end()));
-
-            // 清空队列
-            _current_size = 0;
-            _list.clear();
-
-            // 唤醒压入线程
-            _cv_push.notify_one();
+    int pop(T& v) {
+        std::unique_lock<std::mutex> lock(_mutex);
+        // 当队列为空且队列未被关闭时，等待数据压入
+        _cv_pop.wait(lock, [&]{ return !empty() || _state == State::CLOSED; })
+        if (empty() && _state == State::CLOSED) {
+            return 0;
         }
+        
+        _current_size -= 1;
+        v = _queue.front()
 
-        // 将取出的数据写入 v
-        v = temp_list.front();
+        _cv_push.notify_one();
 
-        return OK;
+        return 1;
     }
 
     // 关闭队列
     void close() noexcept {
         std::unique_lock<std::mutex> lock(_mutex);
-        // 当且仅当所有使用该队列的producer都关闭时，关闭该队列
-        // 应保证任一producer仅且仅能调用close方法一次
-        if (++_n_closed_producer == _n_producer) {
-            _state = State::CLOSED;
-        }
-
-        _cv_pop.notify_all();
+        _state = State::CLOSED;
         _cv_push.notify_all();
+        _cv_pop.notify_all();
     }
 };
 
